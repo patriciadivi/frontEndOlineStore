cdd76642a039119f01d7d28322d692c27a66ba18
Felipe Rangel <feliperangel277@gmail.com>
2022-03-10 17:53:53 -0300

    Requisito 4 - implementa lista de categorias de produtos

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Categories from './Categories';
 
 class Home extends Component {
   render() {
     return (
       <div>
+        <Categories />
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
