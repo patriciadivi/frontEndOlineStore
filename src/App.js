68e7d10ce30433f3bf634c55aa48f65c1ee2b9d9
Sandro Bistene <sandrosalasfilho3@gmail.com>
2022-03-10 17:28:47 -0300

    Requisito 2- Implementa pagina de listagem de produtos vazia

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <p>teste</p>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={ Home } />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
